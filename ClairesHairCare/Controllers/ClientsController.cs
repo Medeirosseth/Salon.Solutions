a7f12639407db68d6708a48c9e3762e1b01c58b8
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-05-21 10:05:06 -0700

    add edit method to ClientController class

--- ClairesHairCare/Controllers/ClientsController.cs
+++ ClairesHairCare/Controllers/ClientsController.cs
@@ -37,7 +37,14 @@ namespace ClairesHairCare.Controllers
 
     public ActionResult Details(int id)
     {
-      Client thisClient = _db.Client.FirstOrDefault(ClientsController => client.ClientId == id);
+      Client thisClient = _db.Clients.FirstOrDefault(ClientsController => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(ClientsController => client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylist, "StylistId", "StylistName");
       return View(thisClient);
     }
 
